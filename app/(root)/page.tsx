f047c03fa4fe89522a018daac466ee498c920148
Himanshi Gupta <101356499+himanshi243@users.noreply.github.com>
2024-06-22 11:03:50 +0530

    banner removed and text changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,31 @@
 import Collections from "@/components/Collections";
 import ProductList from "@/components/ProductList";
-
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Image src="/Banner.png" alt="Banner" width={2000} height={1000} className="w-screen" />
+      <div className="flex items-center mt-4" style={{ height: '400px' }}>
+        <div className="w-1/2 p-4 text-white">
+          <h1 style={{ fontSize: '3rem' }} className="font-bold">From Quarry to Bathroom</h1>
+          <p className="mt-8 text-xl">
+            Transform Your Bathroom into a Luxurious Retreat with our <br /> 
+            Stunning Washbasins.<br />  
+            Elevate your space with Modern Designs and <br /> 
+            Create a Captivating Look.
+          </p>
+        </div>
+        <div className="w-1/2" >
+          <Image
+            src="/banner-img.png"
+            alt="Banner"
+            width={400} // Adjust width to fit the design
+            height={200} // Adjust height to fit the design
+            style={{ height: '550px' }}
+            className="w-full h-auto"
+          />
+        </div>
+      </div>
       <Collections />
       <ProductList />
     </>
@@ -14,4 +33,3 @@ export default function Home() {
 }
 
 export const dynamic = "force-dynamic";
-
