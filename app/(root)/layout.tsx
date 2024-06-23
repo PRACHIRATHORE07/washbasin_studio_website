fbe3191b55614e9d0fb63c9e36e0cbfe15b8f4bd
bhartendra-geek <bhartendrasharma780733@gmail.com>
2024-06-23 23:30:30 +0530

    title and logo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,8 +9,8 @@ import ToasterProvider from "@/lib/providers/ToasterProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Borcelle Store",
-  description: "Borcelle Ecommerce Store",
+  title: "THE WASHBASIN STUDIO",
+  description: "Artful Legacy Washbasins",
 };
 
 export default function RootLayout({
