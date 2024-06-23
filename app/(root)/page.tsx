1b46dd5ee7c67d7a97b872be9a278be48f999a81
Himanshi Gupta <101356499+himanshi243@users.noreply.github.com>
2024-06-23 15:01:48 +0530

    banner changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,13 +3,16 @@ import Collections from "@/components/Collections";
 import ProductList from "@/components/ProductList";
 import FeaturedSection from "@/components/FeaturedSection";
 
+
 export default function Home() {
   return (
     <>
       <div className="flex items-center mt-4" style={{ height: '400px' }}>
-        <div className="w-1/2 p-4 text-white">
-          <h1 style={{ fontSize: '3rem' }} className="font-bold">From Quarry to Bathroom</h1>
-          <p className="mt-8 text-xl">
+        <div className="w-1/2 p-4 text-white ml-8">
+          <h1 className="font-cardo text-4xl font-bold leading-tight" style={{ fontSize: '3rem', fontFamily:'Cardo' }}>
+            From Quarry to <br/> Bathroom
+          </h1>
+          <p className="mt-8 text-lg font-arial" style={{ fontSize: '.90rem', fontFamily:'Cardo' }}>
             Transform Your Bathroom into a Luxurious Retreat with our <br /> 
             Stunning Washbasins.<br />  
             Elevate your space with Modern Designs and <br /> 
@@ -22,6 +25,7 @@ export default function Home() {
             alt="Banner"
             width={400}
             height={550}
+            style={{ height: '550px' }} 
             className="w-full h-auto"
           />
         </div>
